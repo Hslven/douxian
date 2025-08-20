4fe37c7cdaaadc43f2cd484053250df17be59634
刘旭涛 <414599861@qq.com>
2025-08-20 16:25:27 +0800

    feat: 更新多列表

--- next.config.js
+++ next.config.js
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'build', // 👈 自定义输
+  // 出目录名
+  experimental: {
+    turbo: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   async rewrites() {
-    const targetApiUrl = process.env.NEXT_PUBLIC_ENV === 'development'
-      ? "http://douxian.zhuzhu.pro:2285"
-      : "http://prod-cn.your-api-server.com";
+    const targetApiUrl =
+      process.env.NEXT_PUBLIC_ENV === 'development'
+        ? 'http://douxian.zhuzhu.pro:2285'
+        : 'http://prod-cn.your-api-server.com';
+
     return [
       {
-        source: "/api/proxy/:path*", // 前端请求的路径
-        destination: `${targetApiUrl}/:path*`, // 目标接口地址
+        source: '/api/proxy/:path*',
+        destination: `${targetApiUrl}/:path*`,
       },
     ];
   },
