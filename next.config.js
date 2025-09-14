80f05cd7ee1ca48c67048210b900831d9072469c
hslven <865413601@qq.com>
2025-09-14 23:56:44 +0800

    更新代理

--- next.config.js
+++ next.config.js
@@ -7,10 +7,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   async rewrites() {
-    const targetApiUrl =
-      process.env.NEXT_PUBLIC_ENV === 'development'
-        ? 'http://douxian.zhuzhu.pro:2285'
-        : 'http://douxian.zhuzhu.pro:2285';
+    const targetApiUrl = 'http://www.douxian2.cn:2285'
 
     return [
       {
