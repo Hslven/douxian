4bfc41fc970703b93cfc498e813a4ffce8a2e202
hslven <865413601@qq.com>
2025-09-28 18:05:46 +0800

    更新部署脚本

--- rsync.ts
+++ rsync.ts
@@ -12,7 +12,7 @@ const REMOTE_PORT = 22999;
 const REMOTE_PATH = `/data/nginx/${PM2_APP_NAME}`;
 
 const LOCAL_PROJECT_PATH = path.resolve(__dirname, './');
-const FILES_TO_UPLOAD = ['package.json', '.next', 'next.config.js', 'public'];
+const FILES_TO_UPLOAD = ['package.json', '.next', 'next.config.js'];
 
 const BUILD_COMMAND = 'npm run build';
 
@@ -43,12 +43,15 @@ async function uploadFiles() {
 }
 
 async function main() {
+  const startTime = Date.now();
   console.log('====== 📤 开始部署任务 ======\n');
 
   await buildCommand();
   await uploadFiles();
 
+  const endTime = Date.now();
   console.log('====== ✅ 一键上传完成 ======');
+  console.log(`耗时 ${(endTime - startTime) / 1000}s`);
   console.log(new Date().toLocaleString('zh-cn'), '\n');
 }
 
